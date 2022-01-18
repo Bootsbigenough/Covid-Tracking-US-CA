daf1341811af56ccdf962bc0d582bdf03d6ea2f8
Benjamineidum <benjamineidum@gmail.com>
2022-01-18 13:15:25 -0500

    Fetch Covid Data from rapidapi and display on console log

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,35 +1,33 @@
-
-// Get Covid data from UNStats
+// Get Covid data from RapidAPI
 var getCovidData = function() {
-    var apiCovidUrl = "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases2_v1/FeatureServer/2/query?where=1%3D1&outFields=*&outSR=4326&f=json";
-
-    fetch(apiCovidUrl).then(function(response) {
-        // console.log(response);
-        response.json().then(function(data) {
-            console.log(data);
+    //fetch Api
+    fetch("https://covid-193.p.rapidapi.com/statistics", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": "14561f5a37msh441b09bdd5e7becp1db944jsn9f5336dd9806"
+        }
+    })
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data)
+                })
+            } else {
+                console.log('Error >>> ', response.statusText)
+            }
+        })
+            .catch(err => {
+            console.error(err);
         });
-    });
-};
-
-// Get Canada Covid data
-var getCanadaCovidData = function() {
-    var apiVacUrl = "https://api.covid19tracker.ca/summary";
-
-    fetch(apiVacUrl).then(function(response) {
-        console.log(response);
-        response.json().then(function(data) {
-            console.log(data);
-        });
-    });
-};
-
-// mobile menu
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click' , () => {
-    navbarMenu.classList.toggle('is-active');
-})
-
-getCovidData();
-// getCanadaCovidData();
+    };
+    
+    // mobile menu
+    const burgerIcon = document.querySelector('#burger');
+    const navbarMenu = document.querySelector('#nav-links');
+    
+    burgerIcon.addEventListener('click' , () => {
+        navbarMenu.classList.toggle('is-active');
+    })
+    
+    getCovidData()
