ef6a0959c4c2ca7ceec7f0f169d67bc483503d57
Benjamineidum <benjamineidum@gmail.com>
2022-01-18 17:07:37 -0500

    Fetch API covidData for USA and Canada, current and historical data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
-// Get Covid data from RapidAPI
-var getCovidData = function() {
-    //fetch Api
-    fetch("https://covid-193.p.rapidapi.com/statistics", {
+// Get Covid data from RapidAPIgi
+var getUSAData = function() {
+    //fetch USA Covid Data from Api
+    fetch("https://covid-193.p.rapidapi.com/statistics?country=USA", {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "covid-193.p.rapidapi.com",
@@ -21,7 +21,75 @@ var getCovidData = function() {
             console.error(err);
         });
     };
-    
+var getCanadaData = function() {
+     //fetch Canada Covid data from Api
+    fetch("https://covid-193.p.rapidapi.com/statistics?country=Canada", {
+         "method": "GET",
+          "headers": {
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": "14561f5a37msh441b09bdd5e7becp1db944jsn9f5336dd9806"
+            }
+        })
+        .then(function(response) {
+              if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data)
+                 })
+            } else {
+                console.log('Error >>> ', response.statusText)
+            }
+        })
+             .catch(err => {
+            console.error(err);
+        });
+    };
+var getUSAHistory = function() {
+    //Fetch USA Historical Data from API
+    fetch("https://covid-193.p.rapidapi.com/history?country=usa", {
+    	"method": "GET",
+	    "headers": {
+		    "x-rapidapi-host": "covid-193.p.rapidapi.com",
+		    "x-rapidapi-key": "14561f5a37msh441b09bdd5e7becp1db944jsn9f5336dd9806"
+	        }
+        })
+        .then(function(response) {
+            if (response.ok) {
+              response.json().then(function(data) {
+                  console.log(data)
+               })
+          } else {
+              console.log('Error >>> ', response.statusText)
+          }
+      })
+           .catch(err => {
+          console.error(err);
+      });
+  };
+  var getCanadaHistory = function() {
+    //Fetch Canada's Historical Data from API
+    fetch("https://covid-193.p.rapidapi.com/history?country=Canada", {
+    	"method": "GET",
+	    "headers": {
+		    "x-rapidapi-host": "covid-193.p.rapidapi.com",
+		    "x-rapidapi-key": "14561f5a37msh441b09bdd5e7becp1db944jsn9f5336dd9806"
+	        }
+        })
+        .then(function(response) {
+            if (response.ok) {
+              response.json().then(function(data) {
+                  console.log(data)
+               })
+          } else {
+              console.log('Error >>> ', response.statusText)
+          }
+      })
+           .catch(err => {
+          console.error(err);
+      });
+  };
+
+
+
     // mobile menu
     const burgerIcon = document.querySelector('#burger');
     const navbarMenu = document.querySelector('#nav-links');
@@ -30,4 +98,7 @@ var getCovidData = function() {
         navbarMenu.classList.toggle('is-active');
     })
     
-    getCovidData()
+    getUSAData()
+    getCanadaData()
+    getUSAHistory()
+    getCanadaHistory()
